aa71b3e71fdf488c6aaef32081ce136e7b5249d1
Brecert <11599528+Brecert@users.noreply.github.com>
2019-02-20 03:19:41 -0600

    make typing indicators feel more accurate and reponsive

--- src/components/app/RightPanel.vue
+++ src/components/app/RightPanel.vue
@@ -19,7 +19,7 @@
     <news v-else />
     <div class="chat-input-area" v-if="selectedChannelID">
       <div class="message-area">
-        <textarea class="chat-input" ref="input-box" placeholder="Message" @keydown="chatInput" @keyup="messageKeyUp" v-model="message"></textarea>
+        <textarea class="chat-input" ref="input-box" placeholder="Message" @keydown="chatInput" @input="messageKeyUp" v-model="message"></textarea>
         <button :class="{'send-button': true, 'error-send-button': messageLength > 5000}" @click="sendMessage">Send</button>
       </div>
       <div class="info">
