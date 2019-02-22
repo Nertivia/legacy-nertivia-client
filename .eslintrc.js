a27b269d7c0b2f81ed367d8178d4554136940a16
Brecert <11599528+Brecert@users.noreply.github.com>
2019-02-22 16:32:18 -0600

    redid eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        "no-console": "off",
+        "no-unused-vars": "off"
+    }
+};
