7582b234a093a870c295aaf192a4f5cf9e6ba67b
supertiger1234 <saawalkhn123@googlemail.com>
2019-08-22 13:49:55 +0100

    changed some route names

--- src/services/ServerService.js
+++ src/services/ServerService.js
@@ -2,37 +2,37 @@ import {instance, wrapper} from './Api';
 
 export default {
   post ( data )  {
-    return wrapper(instance().post('/server', data));
+    return wrapper(instance().post('/servers', data));
   },
   updateServer (serverID, data) {
-    return wrapper(instance().patch(`/server/${serverID}`, data));
+    return wrapper(instance().patch(`/servers/${serverID}`, data));
   },
   getChannels(serverID) {
-    return wrapper(instance().get(`/server/channels/${serverID}`));
+    return wrapper(instance().get(`/servers/${serverID}/channels`));
   },
   createChannel(serverID, name) {
-    return wrapper(instance().put(`/server/${serverID}/channel`, {name}));
+    return wrapper(instance().put(`/servers/${serverID}/channels`, {name}));
   },
   updateChannel (serverID, channelID, data) {
-    return wrapper(instance().patch(`/server/${serverID}/channels/${channelID}`, data));
+    return wrapper(instance().patch(`/servers/${serverID}/channels/${channelID}`, data));
   },
   deleteChannel (serverID, channelID) {
-    return wrapper(instance().delete(`/server/${serverID}/channels/${channelID}`));
+    return wrapper(instance().delete(`/servers/${serverID}/channels/${channelID}`));
   },
   postInvite (serverID) {
-    return wrapper (instance().post(`/server/${serverID}/invite`))
+    return wrapper (instance().post(`/servers/${serverID}/invite`))
   },
   getInvites (serverID) {
-    return wrapper (instance().get(`/server/${serverID}/invites`))
+    return wrapper (instance().get(`/servers/${serverID}/invites`))
   },
   getInviteDetail (inviteCode) {
-    return wrapper (instance().get(`/server/invite/${inviteCode}`))
+    return wrapper (instance().get(`/servers/invite/${inviteCode}`))
   },
   joinServer (inviteCode) {
-    return wrapper (instance().post(`/server/invite/${inviteCode}`))
+    return wrapper (instance().post(`/servers/invite/${inviteCode}`))
   },
   leaveServer (serverID) {
-    return wrapper (instance().delete(`/server/${serverID}`))
+    return wrapper (instance().delete(`/servers/${serverID}`))
   },
   
 }
