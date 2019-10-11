4e8b50ed4781ec17c7909dd6a7e8f5ef1ae468e7
supertiger1234 <saawalkhn@googlemail.com>
2019-10-11 11:23:59 +0100

    fixed more user error

--- src/views/GDriveCallback.vue
+++ src/views/GDriveCallback.vue
@@ -8,8 +8,9 @@ import settingsService from "../services/settingsService.js";
 export default {
   async mounted() {
     const url = new URL(location.href);
-    const token = url.searchParams.get("token");
+    const token = url.searchParams.get("state");
     const code = url.searchParams.get("code");
+    console.log(url)
     await settingsService.GDriveAuth(code, token);
 
     window.opener.postMessage({ status: 'done' }, "*");
