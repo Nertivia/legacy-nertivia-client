26d1c3f11b015e06652368032e7a3a6aaa1bb357
supertiger1234 <saawalkhn@googlemail.com>
2019-10-31 13:08:18 +0000

    removed inline fence

--- src/utils/messageFormatter.js
+++ src/utils/messageFormatter.js
@@ -1,45 +1,48 @@
-import twemoji from 'twemoji'
-import emojiParser from '@/utils/emojiParser';
+import twemoji from "twemoji";
+import emojiParser from "@/utils/emojiParser";
 import config from "@/config.js";
 
-import customEmoji from './markdown-it-plugins/customEmoji'
-import formatLink from './markdown-it-plugins/formatLink'
-import formatCode from './markdown-it-plugins/formatCode'
-import normalizeFence from './markdown-it-plugins/normalizeFence'
-import inlineFence from './markdown-it-plugins/inlineFence'
+import customEmoji from "./markdown-it-plugins/customEmoji";
+import formatLink from "./markdown-it-plugins/formatLink";
+import formatCode from "./markdown-it-plugins/formatCode";
+import normalizeFence from "./markdown-it-plugins/normalizeFence";
 
-import hljs from 'highlight.js'
+import hljs from "highlight.js";
 
-import MarkdownIt from 'markdown-it'
-import chatPlugin from 'markdown-it-chat-formatter/dist-src/plugin'
+import MarkdownIt from "markdown-it";
+import chatPlugin from "markdown-it-chat-formatter/dist-src/plugin";
 
 const markdown = new MarkdownIt({
-  highlight: function (str, lang) {
+  highlight: function(str, lang) {
     if (lang && hljs.getLanguage(lang)) {
       try {
-        return '<div class="codeblock"><code>' +
-               hljs.highlight(lang, str, true).value +
-               '</code></div>';
+        return (
+          '<div class="codeblock"><code>' +
+          hljs.highlight(lang, str, true).value +
+          "</code></div>"
+        );
       } catch (err) {
-				console.error(err)
+        console.error(err);
       }
     }
 
-    return '<div class="codeblock"><code>' + markdown.utils.escapeHtml(str) + '</code></div>';
+    return (
+      '<div class="codeblock"><code>' +
+      markdown.utils.escapeHtml(str) +
+      "</code></div>"
+    );
   }
 })
   .use(normalizeFence)
   .use(chatPlugin)
-	.use(customEmoji)
+  .use(customEmoji)
   .use(formatLink)
-  .use(formatCode)
-  .use(inlineFence)
+  .use(formatCode);
 
-export default (message) => {
+export default message => {
+  message = markdown.render(message).trim();
 
-	message = markdown.render(message).trim();
+  message = emojiParser.replaceEmojis(message);
 
-	message = emojiParser.replaceEmojis(message);
-
-	return message;
-}
+  return message;
+};
