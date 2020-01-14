21db3c27c6d32da7d8921bd4b5740e79d4c23c17
jasonthekitten <jason05gronn@gmail.com>
2020-01-14 14:30:28 +0000

    Allow specifying a specific server in configs\nShould not affect existing setups

--- src/services/customEmoji.js
+++ src/services/customEmoji.js
@@ -1,5 +1,10 @@
 import { instance, wrapper } from "./Api";
 
+const config = require('../config.js');
+
+let domain = "";
+if (config.serverURL) domain = config.serverURL+"/"
+
 export default {
   post(data, onProgress) {
     const url = `/settings/emoji`;
@@ -15,14 +20,14 @@ export default {
         return percentCompleted;
       }
     };
-    return wrapper(instance().post(url, data, config));
+    return wrapper(instance().post(domain+url, data, config));
   },
   delete(emojiID) {
-    return wrapper(instance().delete(`/settings/emoji`, { data: { emojiID } }));
+    return wrapper(instance().delete(domain+`/settings/emoji`, { data: { emojiID } }));
   },
   put(data) {
     return wrapper(
-      instance().put(`/settings/emoji`, {
+      instance().put(domain+`/settings/emoji`, {
         emojiID: data.emojiID,
         name: data.name
       })
--- src/services/exploreService.js
+++ src/services/exploreService.js
@@ -1,37 +1,42 @@
 import { instance, wrapper } from "./Api";
 
+const config = require('../config.js');
+
+let domain = "";
+if (config.serverURL) domain = config.serverURL+"/"
+
 export default {
   // servers
   getServersList(params) {
-    return wrapper(instance().get(`explore/servers${params || ""}`));
+    return wrapper(instance().get(domain+`explore/servers${params || ""}`));
   },
   getServer(server_id) {
-    return wrapper(instance().get(`explore/servers/${server_id}`));
+    return wrapper(instance().get(domain+`explore/servers/${server_id}`));
   },
   deleteServer(server_id) {
-    return wrapper(instance().delete(`explore/servers/${server_id}`));
+    return wrapper(instance().delete(domain+`explore/servers/${server_id}`));
   },
   updateServer(server_id, data) {
-    return wrapper(instance().patch(`explore/servers/${server_id}`, data));
+    return wrapper(instance().patch(domain+`explore/servers/${server_id}`, data));
   },
   addServersList(data) {
-    return wrapper(instance().post(`explore/servers`, data));
+    return wrapper(instance().post(domain+`explore/servers`, data));
   },
 
   // themes
   getThemes() {
-    return wrapper(instance().get(`explore/themes`));
+    return wrapper(instance().get(domain+`explore/themes`));
   },
   getTheme(id) {
-    return wrapper(instance().get(`explore/themes/${id}`));
+    return wrapper(instance().get(domain+`explore/themes/${id}`));
   },
   addTheme(id, data) {
-    return wrapper(instance().post(`explore/themes/${id}`, data));
+    return wrapper(instance().post(domain+`explore/themes/${id}`, data));
   },
   updateTheme(id, data) {
-    return wrapper(instance().patch(`explore/themes/${id}`, data));
+    return wrapper(instance().patch(domain+`explore/themes/${id}`, data));
   },
   applyTheme(id) {
-    return wrapper(instance().get(`explore/themes/${id}/apply`));
+    return wrapper(instance().get(domain+`explore/themes/${id}/apply`));
   }
 };
