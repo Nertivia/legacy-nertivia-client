3a9553df5ab749a22f76b98e9095fb08b6ab0aa4
supertiger1234 <saawalkhn@googlemail.com>
2020-04-23 16:15:15 +0100

    fixed more bugs -_- 🥱🥱

--- src/utils/markdown-rules/link.js
+++ src/utils/markdown-rules/link.js
@@ -3,25 +3,29 @@ import * as SimpleMarkdown from "simple-markdown";
 export default order => {
   return {
     order: order++,
-    match: function(source) {
+    match: function (source) {
       return /^(^|\s)(https?:\/\/)?(([^.\s/<>]+\.)+[a-z]+(:(\d+))?(\/[^\s<>]*)?)/.exec(source);
     },
 
-    parse: function(capture) {
+    parse: function (capture) {
       return {
         protocol: capture[2],
         url: capture[0]
       };
     },
-    html: function(node) {
-      const url = new URL(node.protocol ? node.url.trim() : 'https://' + node.url.trim());
-      const tdl = url.hostname.split(".").pop();
-      if (tdl.length >= 4 || tdl.length <=1) return node.url;
-      return SimpleMarkdown.htmlTag("a",  node.protocol ? url.href: url.href.split("//").pop() , {
-        href: SimpleMarkdown.sanitizeText(SimpleMarkdown.sanitizeUrl(url.href)),
-        class: "link",
-        target: "_blank"
-      }) + " ";
+    html: function (node) {
+      try {
+        const url = new URL(node.protocol ? node.url.trim() : 'https://' + node.url.trim());
+        const tdl = url.hostname.split(".").pop();
+        if (tdl.length >= 4 || tdl.length <= 1) return node.url;
+        return SimpleMarkdown.htmlTag("a", node.protocol ? url.href : url.href.split("//").shift(), {
+          href: SimpleMarkdown.sanitizeText(SimpleMarkdown.sanitizeUrl(url.href)),
+          class: "link",
+          target: "_blank"
+        }) + " ";
+      } catch {
+        return node.url
+      }
     }
   };
 };
