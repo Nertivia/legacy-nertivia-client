86f0ff490cdd61be19f2d09b2f5b8746ba52d63a
supertiger1234 <saawalkhn@googlemail.com>
2020-04-23 16:22:14 +0100

    fixed more buuugs

--- src/utils/markdown-rules/link.js
+++ src/utils/markdown-rules/link.js
@@ -18,7 +18,8 @@ export default order => {
         const url = new URL(node.protocol ? node.url.trim() : 'https://' + node.url.trim());
         const tdl = url.hostname.split(".").pop();
         if (tdl.length >= 4 || tdl.length <= 1) return node.url;
-        return SimpleMarkdown.htmlTag("a", node.protocol ? url.href : url.href.split("//").shift(), {
+        const urlArr = url.href.split("//");
+        return SimpleMarkdown.htmlTag("a", node.protocol ? url.href : urlArr.shift() + "//" + urlArr.join("//"), {
           href: SimpleMarkdown.sanitizeText(SimpleMarkdown.sanitizeUrl(url.href)),
           class: "link",
           target: "_blank"
