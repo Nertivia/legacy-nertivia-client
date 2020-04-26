17b8582b32275ebd4daa6c31aae233cca3dbd9b2
supertiger1234 <saawalkhn@googlemail.com>
2020-04-26 20:49:18 +0100

    removed recaptcha

--- src/components/global/Recaptcha.vue
+++ src/components/global/Recaptcha.vue
@@ -1,33 +1,19 @@
 <template>
-  <vue-recaptcha
-    ref="recaptcha"
-    :sitekey="sitekey"
-    theme="dark"
-    @verify="submit"
-  />
+  <h-captcha :sitekey="sitekey" theme="dark" ref="recaptcha" @verify="submit" />
 </template>
 
 <script>
-import VueRecaptcha from "vue-recaptcha";
+import hCaptcha from "@hcaptcha/vue-hcaptcha";
+
 import config from "@/config.js";
 export default {
-  components: { VueRecaptcha },
+  components: { hCaptcha },
   data() {
     return {
-      sitekey: config.recaptcha
+      sitekey: config.captchaSiteKey
     };
   },
-  beforeMount() {
-    const existing = document.getElementById("reCaptchaScript");
-    if (existing) existing.remove();
 
-    const $script = document.createElement("script");
-    $script.async = true;
-    $script.id = "reCaptchaScript";
-    $script.src =
-      "https://www.google.com/recaptcha/api.js?onload=vueRecaptchaApiLoaded&render=explicit";
-    document.head.appendChild($script);
-  },
   methods: {
     submit(response) {
       this.$emit("verify", response);
